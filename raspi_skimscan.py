bc525245701e66aef626eab1fdd9a7136b05d40a
photoresistor <twinegarner@gmail.com>
2018-05-15 22:23:55 -0700

    Update raspi_skimscan.py

--- raspi_skimscan.py
+++ raspi_skimscan.py
@@ -18,7 +18,7 @@ from PIL import ImageFont
 
 
 # Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
+RST = 24     # on the PiOLED this pin isnt used
 # Note the following are only used with SPI:
 DC = 23
 SPI_PORT = 0
