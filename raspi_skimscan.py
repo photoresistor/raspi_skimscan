c6b0205322136cae8ee62bc0661890589f430de8
NachiThePanda <70688716+NachiThePanda@users.noreply.github.com>
2022-11-16 12:03:29 -0500

    Update raspi_skimscan.py
    Reformatted text to prevent TabError and put parentheses for the print function

--- raspi_skimscan.py
+++ raspi_skimscan.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # file: raspi_skimscan.py
 # auth: Tyler Winegarner (twinegarner@gmail.com)
 # desc: scans for local bluetooth devices with names matching the description of those
-#       used in cas pump credit card skimmers. This software is directly derived from 
+#       used in cas pump credit card skimmers. This software is directly derived from
 #       the research done by Nathan Seidle as documented in this article:
 #       https://learn.sparkfun.com/tutorials/gas-pump-skimmers
 #
@@ -16,15 +18,18 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-
 # Raspberry Pi pin configuration:
-RST = 24     # on the PiOLED this pin isnt used
+
+RST = 24  # on the PiOLED this pin isnt used
+
 # Note the following are only used with SPI:
+
 DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC,
+        spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 disp.begin()
 
 time.sleep(3)
@@ -37,38 +42,38 @@ image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 
 font = ImageFont.load_default()
-ellipsis = ".   "
+ellipsis = '.   '
 phase = 0
 
 while True:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((0, 24), "scanning" + ellipsis, font=font, fill=255)
-        disp.image(image)
-        disp.display()
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((0, 24), 'scanning' + ellipsis, font=font, fill=255)
+    disp.image(image)
+    disp.display()
 
-	nearby_devices = bluetooth.discover_devices(duration=10, lookup_names=True)
+    nearby_devices = bluetooth.discover_devices(duration=10,
+            lookup_names=True)
 
-	print("found %d devices" % len(nearby_devices))
+    print('found %d devices' % len(nearby_devices))
 
-	for addr, name in nearby_devices:
-		if (name == "HC-05") or (name == "HC-03") or (name == "HC-06"):
-			draw.rectangle((0, 0, width, height), outline=0, fill=0)
-			draw.text((0, 12), "Potential skimmer", font=font, fill=255)
-			draw.text((0, 24), name + " found.", font=font, fill=255)
-			draw.text((0, 36), "Skip this pump.", font=font, fill=255)
+    for (addr, name) in nearby_devices:
+        if name == 'HC-05' or name == 'HC-03' or name == 'HC-06':
+            draw.rectangle((0, 0, width, height), outline=0, fill=0)
+            draw.text((0, 12), 'Potential skimmer', font=font, fill=255)
+            draw.text((0, 24), name + ' found.', font=font, fill=255)
+            draw.text((0, 36), 'Skip this pump.', font=font, fill=255)
 
-			disp.image(image)
-			disp.display()
-			time.sleep(5)
+            disp.image(image)
+            disp.display()
+            time.sleep(5)
 
-	phase += 1
-	if phase == 1:
-		ellipsis = "..  "
-	elif phase == 2:
-		ellipsis = "... "
-	elif phase == 3:
-		ellipsis = "...."
-	else:
-		ellipsis = ".   "
-		phase = 0
-
+    phase += 1
+    if phase == 1:
+        ellipsis = '..  '
+    elif phase == 2:
+        ellipsis = '... '
+    elif phase == 3:
+        ellipsis = '....'
+    else:
+        ellipsis = '.   '
+        phase = 0
